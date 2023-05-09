47efdd1b392d33505011b4449f46f0a452eefaee
Siddhant Rath <sid@tamu.edu>
2023-05-09 14:47:39 -0500

    Initial

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+
+export default () => {
+  return (
+    <div>
+      <h1>Container App</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
