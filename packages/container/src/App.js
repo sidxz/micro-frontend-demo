7d0bc9f40a57d6725195bf9609cb8235f078d02f
Siddhant Rath <sid@tamu.edu>
2023-05-10 13:18:23 -0500

    container deploy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Container App!</h1>
+      <h1>Container App</h1>
       <hr />
       <MarketingApp />
     </div>
